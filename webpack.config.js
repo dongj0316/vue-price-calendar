72b22697f1d7f4e5aaf48aa13657b8096ab29d14
dongj0316 <475586651@qq.com>
2018-05-17 15:55:17 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var webpack = require('webpack')
 
 module.exports = {
   entry: './src/main.js',
+  // entry: './src/vue-price-calendar/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
